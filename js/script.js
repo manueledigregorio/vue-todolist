697d3124aeb3488d3f316be032a9da789806e5f4
manueledigregorio <dig.manuele@gmail.com>
2023-09-29 19:18:44 +0200

    [add] task  remuve

--- js/script.js
+++ js/script.js
@@ -2,13 +2,24 @@ const { createApp } = Vue;
 createApp({
   data(){
     return{
+      tasks:[
+        'fare la spesa ',
+        'dare da mangiare al cane',
+        'andare in America'
+      ],
 
-
-
+       
     }
   },
+  
   methods:{
-    
+    remuveTask(i){
+      this.tasks.splice(i, 1)
+
+    }
+
   }
 
-}).mount('#app')
+
+
+}).mount('#app');
